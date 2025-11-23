2530ca457f522eb7c454e7ae88f49acf6f91d601
GitMenglin <mzl20070124@gmail.com>
2025-11-23 01:09:42 -0500

    Revert "fix backend retrieving logic"
    This reverts commit 46cd87383b0e9b53e3ada167aa2228bb227d4a2b.

--- app_firestore.js
+++ app_firestore.js
@@ -436,7 +436,7 @@ function computeRiskScore(sensors, idx) {
 function flattenPacketDocs(packetDocs) {
   if (!packetDocs.length) return [];
 
-  const docs = [...packetDocs].reverse(); // copy
+  const docs = [...packetDocs]; // copy
   const now  = Date.now();
   const series = [];
 
