2f3d4292d3dad490e551701d18a8266f6ee1a2ae
GitMenglin <mzl20070124@gmail.com>
2025-11-22 22:41:08 -0500

    try this?

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyAzZvd---",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAz-ZVd--bf83eo1OcUALI4KtXt_kXKCPQ",
   authDomain: "ece198-d2f99.firebaseapp.com",
   projectId: "ece198-d2f99",
   storageBucket: "ece198-d2f99.firebasestorage.app",
-  messagingSenderId: "553329796893",
-  appId: "1:553329796893:web:4e1bacca90304c480f0c74",
-  measurementId: "G-61PP6GTXMH"
+  messagingSenderId: "535239796893",
+  appId: "1:535239796893:web:4e1abcca90304c480f0c74",
+  measurementId: "G-61PPG6TXMH"
 };
